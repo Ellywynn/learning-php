7b8d2a7203b99cd015a535f69fca8e541e6208ac
Ellywynn <whatisyouandme@mail.ru>
2021-02-26 22:20:41 +0300

    secure working form with mysql connection

--- mysql.php
+++ mysql.php
@@ -13,43 +13,125 @@
 
     <?php
 
-    // declare global variables
-    // defaults
-    $name = $age = $surname = "";
-    // errors
-    $nameError = $surnameError = $ageError = "";
+    // declare global variables and set them to empty values
+    $nickname = $email = $password = $telephone = "";
+    // error variables
+    $nicknameError = $emailError = $passwordError = $telephoneError = "";
+
+    // checks for form validation
+    $success = true;
 
     // if there is POST request, check form
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // if name is empty, require it
-        if (empty($_POST['name'])) {
-            $nameError = "Name is required";
+        if (empty($_POST['nickname'])) {
+            $nicknameError = "Nickname is required";
+            $success = false;
         } else {
-            // else check name and match with regex
-            $name = checkInput($_POST['name']);
+            // else check nickname and match with regex
+            $nickname = checkInput($_POST['nickname']);
 
-            if (!preg_match("/^[a-zA-Z]/", $name))
-                $nameErr = "Only letters and white space allowed";
+            if (!preg_match("/^[a-zA-Z0-9]{4,20}/", $nickname)) {
+                $nicknameError = "Only digits and letters allowed(at least 4)";
+                $success = false;
+            }
         }
 
-        if (empty($_POST['surname'])) {
-            $surnameError = "Surname is required";
+        // email validation
+        if (empty($_POST['email'])) {
+            $emailError = "Email is required";
+            $success = false;
         } else {
-            $surname = checkInput($_POST['surname']);
+            $email = checkInput($_POST['email']);
 
-            if (!preg_match("/^[a-zA-Z]/", $surname))
-                $surnameError = "Only letters and white space allowed";
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailError = "Invalid email format";
+                $success = false;
+            }
         }
 
-        if (empty($_POST['age'])) {
-            $ageError = "Age is required";
+        if (empty($_POST['password'])) {
+            $passwordError = "Password is required";
+            $success = false;
         } else {
-            $age = checkInput($_POST['age']);
+            $password = checkInput($_POST['password']);
 
-            if (!preg_match("/^[0-9]/", $age))
-                $ageError = "Only numbers are allowed";
+            if (!preg_match("/^[a-zA-Z0-9]{6,20}$/", $password)) {
+                $passwordError = "Password must be 6-20 letters";
+                $success = false;
+            }
+        }
+
+        if (empty($_POST['telephone'])) {
+            $telephoneError = "Password is required";
+            $success = false;
+        } else {
+            $telephone = checkInput($_POST['telephone']);
+
+            if (!preg_match("/^[0-9]{10}$/", $telephone)) {
+                $telephoneError = "Only digits are allowed";
+                $success = false;
+            }
+        }
+
+        // if form is valid, save information into the database
+        if ($success) {
+            /* database connection */
+
+            // db config
+            $dbserver = "localhost";
+            $dbusername = "root";
+            $dbpassword = "";
+            $dbname = "test";
+            $dbport = 3306;
+
+            try {
+                // try to connect 
+                $pdo = new PDO("mysql:host=$dbserver;port=$dbport;dbname=$dbname", $dbusername, $dbpassword);
+
+                // set the error mode
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $pdo->exec("CREATE DATABASE IF NOT EXISTS $dbname");
+                $pdo->exec("CREATE TABLE IF NOT EXISTS persons(
+        personId INT NOT NULL AUTO_INCREMENT,
+        nickname VARCHAR(40) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(80) NOT NULL,
+        telephone VARCHAR(12) NOT NULL,
+        PRIMARY KEY(personId)
+    )");
+
+                $statement = $pdo->prepare("INSERT INTO persons(nickname, email, password, telephone)
+            VALUES (:nickname, :email, :password, :telephone)");
+
+                $encryptedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+                $statement->bindParam(':nickname', $nickname);
+                $statement->bindParam(':email', $email);
+                $statement->bindParam(':password', $encryptedPassword);
+                $statement->bindParam(':telephone', $telephone);
+
+                $statement->execute();
+
+                // $statement = $pdo->prepare("SELECT password FROM persons WHERE nickname='ellywynn'");
+                // $statement->execute();
+
+                // $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
+
+                // if (password_verify("abcd1234", $statement->fetchAll()[0]['password'])) {
+                //     echo "<h2>PASSWORD VERIFIED</h2>";
+                // }
+
+                // close the connection
+                $pdo = null;
+            } catch (PDOException $e) {
+                // if there is any error, print it
+                echo "Connection failed: " . $e->getMessage();
+            }
         }
     }
+
 
     // checks user input
     function checkInput($element)
@@ -75,80 +157,23 @@
 
     <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
         <div class="error">* - required fields</div> <br>
-        Name: <input type="text" name="name" value="<?php echo $name ?>">
-        <span class="error">* <?php echo $nameError; ?></span>
+        Nickname: <input type="text" name="nickname" value="<?php echo $nickname ?>" autocomplete="off" maxlength="20">
+        <span class="error">* <?php echo $nicknameError; ?></span>
         <br> <br>
 
-        Surname: <input type="text" name="surname" value="<?php echo $surname ?>">
-        <span class="error">* <?php echo $surnameError; ?></span>
+        Email: <input type="text" name="email" value="<?php echo $email ?>" autocomplete="off">
+        <span class="error">* <?php echo $emailError; ?></span>
         <br> <br>
 
-        Age: <input type="text" name="age" value="<?php echo $age ?>">
-        <span class="error">* <?php echo $ageError; ?></span>
+        Password: <input type="password" name="password" value="<?php echo $password ?>" autocomplete="off" maxlength="20">
+        <span class="error">* <?php echo $passwordError; ?></span>
+        <br> <br>
+
+        Telephone: <input type="text" name="telephone" value="<?php echo $telephone ?>" autocomplete="off" maxlength="10">
+        <span class="error">* <?php echo $telephoneError; ?></span>
         <br> <br>
         <input type="submit" name="submit" value="Submit">
     </form>
-
-    <?php
-
-    /* database connection */
-
-    // db config
-    $dbserver = "localhost";
-    $dbusername = "root";
-    $dbpassword = "kitsuNYA6996";
-    $dbname = "test";
-    $dbport = 3306;
-
-    try {
-        // try to connect 
-        $pdo = new PDO("mysql:host=$dbserver;port=$dbport;dbname=$dbname", $dbusername, $dbpassword);
-
-        // set the error mode
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $pdo->exec("CREATE DATABASE IF NOT EXISTS $dbname");
-        $pdo->exec("CREATE TABLE IF NOT EXISTS persons(
-            personId INT NOT NULL AUTO_INCREMENT,
-            nickname VARCHAR(40) NOT NULL,
-            email VARCHAR(255) NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            telephone VARCHAR(12) NOT NULL,
-            PRIMARY KEY(personId)
-        )");
-
-        //  $statement = $pdo->prepare("INSERT INTO persons(nickname, email, password, telephone)
-        //  VALUES (:nickname, :email, :password, :telephone)");
-
-        // $statement->bindParam(':nickname', $nickname);
-        // $statement->bindParam(':email', $email);
-        // $statement->bindParam(':password', $password);
-        // $statement->bindParam(':telephone', $telephone);
-
-        // $nickname = 'ellywynn';
-        // $email = 'whatisyouandme@mail.ru';
-        // $password = password_hash('abcd1234', PASSWORD_DEFAULT);
-        // $telephone = '12341234';
-        // $statement->execute();
-
-        $statement = $pdo->prepare("SELECT password FROM persons WHERE nickname='ellywynn'");
-        $statement->execute();
-
-        $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
-
-        if (password_verify("abcd1234", $statement->fetchAll()[0]['password'])) {
-            echo "<h2>PASSWORD VERIFIED</h2>";
-        }
-
-
-        // close the connection
-        $pdo = null;
-    } catch (PDOException $e) {
-        // if there is any error, print it
-        echo "Connection failed: " . $e->getMessage();
-    }
-    ?>
-
 </body>
 
 </html>
